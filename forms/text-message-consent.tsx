5cb285e8f4ef682b49119af999cc390234e873ad
Stephen Walther <stephen.walther@superexpert.com>
2025-03-06 20:07:06 -0600

    consent saved to database

--- forms/text-message-consent.tsx
+++ forms/text-message-consent.tsx
@@ -1,11 +1,13 @@
 "use client";
+import { saveConsentAction } from "@/forms/form-actions";
 
 export const TextMessageConsentContent = ({
   onSubmit,
 }: {
   onSubmit: (result: string) => void;
 }) => {
-  const handleYes = () => {
+  const handleYes = async () => {
+    await saveConsentAction("text-message-consent");
     onSubmit("Yes, the user has agreed to receive text messages.");
   };
   const handleNo = () => {
@@ -18,7 +20,7 @@ export const TextMessageConsentContent = ({
       <p>Do you consent to receive text messages from us?</p>
       <div className="mt-4 flex gap-3">
         <button className="btn btnPrimary" onClick={handleYes}>Yes</button>
-        <button className="btn btnCancel" style={{ marginLeft: '0.75rem' }}  onClick={handleNo}>No</button>
+        <button className="btn btnCancel ml-3" onClick={handleNo}>No</button>
       </div>
     </div>
   );
