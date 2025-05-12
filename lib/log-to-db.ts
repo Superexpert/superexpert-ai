c1b79c2cbd52b06e099e8ca41f0fed2fb74fa708
Stephen Walther <stephen.walther@superexpert.com>
2025-05-12 13:06:44 -0500

    Missed an implicit any

--- lib/log-to-db.ts
+++ lib/log-to-db.ts
@@ -2,7 +2,7 @@ import { logStream } from '@superexpert-ai/framework/server';
 import { Prisma } from '@prisma/client';
 import { DBService } from '@/lib/db/db-service';
 
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 logStream.on('data', async (row: any) => {
   const { time, userId, agentId, component, level, msg, ...rest } = row;
 
